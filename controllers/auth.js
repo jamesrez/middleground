3f07f1905e6d64b0603f31591f97a9fae9192763
James Rezendes <James@MacBook-Pro.local>
2017-11-14 12:17:13 -0800

    please

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,7 +14,7 @@ module.exports = function(app, passport) {
 
   app.get('/auth/facebook', passport.authenticate('facebook', { session: false }));
 
-  app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }),
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/', session: false }),
     function(req, res) {
       // Successful authentication, redirect home.
       res.redirect('/');
